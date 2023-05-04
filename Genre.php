ca04490a3bba7e1b287d6e384f440d9ebafb7c43
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-05-04 16:37:50 +0200

    adding genres

--- Genre.php
+++ Genre.php
@@ -1,6 +1,6 @@
 <?php
 
-    class Producer {
+    class Genre {
         public $type;
 
 
--- index.php
+++ index.php
@@ -3,46 +3,79 @@
     class Movie {
         public $name;
         public $score;
-        public $length;
-        public $producers;
+        public $length;    
+        public $genres;
 
-        public function __construct($name, $score, $length, $producer)
+        public function __construct($name, $score, $length, $producer, array $genres)
         {
             $this->name = $name;
             $this->score = $score;
             $this->length = $length;
             $this->producer = $producer;
+
+            
+            foreach ($genres as $genre) {
+                if (!$genre instanceof Genre) {
+                    echo 'Ogni genere deve essere una instanza della classe Genre!';
+                    die();
+                }
+            }
+            
         }
 
         public function getInfo() {
             return $this->name . " " . $this->score . " " . $this->length . " " . $this->producer;
         }
         
+        public function getGenre() {
+            $generi = '';
+
+            foreach($this->genres as $genre) {
+                $generi .= $genre->type;
+            }
+
+            return $generi;
+        }
         
     }
-    /*
-    require __DIR__ . '/Producer.php';
+    
+    require __DIR__ . '/Genre.php';
 
-    $genre = [
-        new Genre(),
-        new Genre(),
-        new Genre()
+    $genreAvengers = [
+        new Genre('SuperHeros'),
+        new Genre('Action'),
+        new Genre('Fighter')
     ];
-    */
-    $avengers = new Movie('Avengers', 3.5, '3 hours', 'Marvel Studios');
 
-    $avatar = new Movie('Avatar', 4.5, '2.5 hours', 'James Cameron');
+    $avengers = new Movie('Avengers', 3.5, '3 hours', 'Marvel Studios', $genreAvengers);
 
-    $pirates_of_the_caribbean = new Movie('Pirates of the Caribbean', 5, '1.5 hours', 'Jerry Bruckheimer');
+    $genreAvatar = [
+        new Genre('Exploration'),
+        new Genre('Research'),
+        new Genre('Simulator')
+    ];
+
+    $avatar = new Movie('Avatar', 4.5, '2.5 hours', 'James Cameron', $genreAvatar);
+
+    $genreCaribbean = [
+        new Genre('Pirates'),
+        new Genre('Journey'),
+        new Genre('Research')
+    ];
+
+    $pirates_of_the_caribbean = new Movie('Pirates of the Caribbean', 5, '1.5 hours', 'Jerry Bruckheimer', $genreCaribbean);
 
     var_dump($avengers);
 
     echo $avengers->getInfo();
+    echo $avengers->getGenre();
     ?>
     <br>
     <?php
     echo $avatar->getInfo();
+    echo $avatar->getGenre();
     ?>
     <br>
     <?php
     echo $pirates_of_the_caribbean->getInfo();
+    echo $pirates_of_the_caribbean->getGenre();
